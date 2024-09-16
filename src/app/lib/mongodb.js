e06b149554a0afa5991143b80b8cf531c918611b
md niyaz <mdniyaz1842000@gmail.com>
2024-09-16 17:21:47 +0530

    2nd commit by Niyaz

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -12,13 +12,13 @@ let clientPromise;
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable to ensure the MongoClient is not instantiated more than once
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
   clientPromise = client.connect();
 }
 
